264c38efd5da7f9356c95f529594791ffa3864c9
Berkaj Tilmac <berkaj.102517@student.ugd.edu.mk>
2023-10-22 18:27:01 +0300

    Proje dosyası ekle.

---
+++ ecommerce-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ecommerce_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
